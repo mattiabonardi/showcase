0ae487f8abd3d8bb4c7a9625fd90f8614b017f31
Mattia Bonardi <mattiabonardi99@gmail.com>
2022-06-22 16:15:22 +0200

    Add react material

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,28 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import React from "react";
+import { CacheProvider, EmotionCache } from "@emotion/react";
+import { ThemeProvider, CssBaseline } from "@mui/material";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+import createEmotionCache from "../utility/createEmotionCache";
+import lightTheme from "../styles/theme/light";
+import "../styles/globals.css";
 
-export default MyApp
+const clientSideEmotionCache = createEmotionCache();
+
+const MyApp = (props: {
+  Component: any;
+  emotionCache?: EmotionCache | undefined;
+  pageProps: any;
+}) => {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+
+  return (
+    <CacheProvider value={emotionCache}>
+      <ThemeProvider theme={lightTheme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  );
+};
+
+export default MyApp;
